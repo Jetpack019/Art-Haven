a07562639a32afc216ace1f862558b72df0945a1
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2023-08-11 16:57:37 +0800

    Update about.js

--- about.js
+++ about.js
@@ -63,9 +63,6 @@ function closeSubmenu(e) {
   
   
 
-
-
-
 // Shop Cart
 // Only code for the counter of cart
 // Retrieve cart data from localStorage
@@ -95,3 +92,23 @@ generateCartItems(cartItems);
 
 // Call the function to update the cart count
 updateCartCount(cartItems);
+
+
+// For animation scroll
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
