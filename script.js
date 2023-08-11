eaef76ec5423ba21a13ca5e5dcf3776acd75e524
JadeRoss <105959739+Jetpack019@users.noreply.github.com>
2023-08-11 16:56:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -247,3 +247,23 @@ generateCartItems(cartItems);
 
 // Call the function to update the cart count
 updateCartCount(cartItems);
+
+
+// For animation scroll
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
